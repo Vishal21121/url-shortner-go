8cca0c4e1fbb13506777e3d61c3c829b3d5cf0c7
Vishal21121 <vs84077000@gmail.com>
2024-07-01 22:15:10 +0530

    added: code in UserLinks.jsx for creating the links using Dialog of shadcn

--- client/src/context/UrlContextProvider.jsx
+++ client/src/context/UrlContextProvider.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { UrlContext } from "./urlContext";
+import toast from "react-hot-toast";
 
 function UrlContextProvider({ children }) {
   const [urls, setUrls] = useState(null);
@@ -17,8 +18,55 @@ function UrlContextProvider({ children }) {
     }
   };
 
+  const createUrl = async (value) => {
+    console.log("called", value);
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_SERVER_URI}/api/v1/urls/create`,
+        {
+          method: "POST",
+          mode: "cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(value),
+        }
+      );
+      const data = await response.json();
+      if (data.data.statusCode === 201) {
+        console.log(data.data);
+        setUrls([...urls, data.data.data]);
+        toast.success("Url created successfully", {
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+      } else if (data.data.statusCode === 409) {
+        toast.error("Please enter another aliase", {
+          style: {
+            borderRadius: "10px",
+            background: "#333",
+            color: "#fff",
+          },
+        });
+      }
+      console.log(data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <UrlContext.Provider value={{ urls, setUrls, fetchUrls }}>
+    <UrlContext.Provider
+      value={{
+        urls,
+        setUrls,
+        fetchUrls,
+        createUrl,
+      }}
+    >
       {children}
     </UrlContext.Provider>
   );
--- client/src/pages/UserLinks.jsx
+++ client/src/pages/UserLinks.jsx
@@ -1,15 +1,56 @@
 import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { CloudFog, Filter } from "lucide-react";
+import { Filter } from "lucide-react";
 import UrlCard from "@/components/UrlCard";
 import { useUrlContext } from "@/context/urlContext";
 import { useUserContext } from "@/context/userContext";
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogFooter,
+} from "@/components/ui/dialog";
+import { DialogClose } from "@radix-ui/react-dialog";
+import z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 function UserLinks() {
-  const { fetchUrls, urls } = useUrlContext();
+  const { fetchUrls, urls, createUrl } = useUrlContext();
   const { user } = useUserContext();
   const [inputBox, setInputBox] = useState("");
-  const [localUrls, setLocalUrls] = useState(urls);
+  const [localUrls, setLocalUrls] = useState(null);
+
+  const urlFormSchema = z.object({
+    aliase: z.string({ required_error: "Please provide aliase" }),
+    url: z.string({ required_error: "Please provide url" }),
+  });
+
+  const urlForm = useForm({
+    resolver: zodResolver(urlFormSchema),
+  });
+
+  const handleUrlForm = (value) => {
+    createUrl({
+      aliase: value.aliase,
+      redirectUrl: value.url,
+      userId: user.data.data._id,
+    });
+    urlForm.reset({
+      aliase: "",
+      url: "",
+    });
+  };
 
   function debounce(func, wait) {
     let timeout;
@@ -35,6 +76,7 @@ function UserLinks() {
 
   useEffect(() => {
     fetchUrls(user.data.data._id);
+    setLocalUrls(urls);
     console.log(user);
   }, []);
 
@@ -43,7 +85,7 @@ function UserLinks() {
       <div className="flex w-full justify-center gap-4">
         <div className="w-1/2 p-4 border rounded-lg">
           <p className="text-xl">Links Created</p>
-          <p className="text-lg">1</p>
+          <p className="text-lg">{urls && urls?.length}</p>
         </div>
         <div className="w-1/2 p-4 border rounded-lg">
           <p className="text-xl">Total Clicks</p>
@@ -53,7 +95,49 @@ function UserLinks() {
       <div className="flex flex-col gap-4 mt-8">
         <div className="flex justify-between">
           <p className="text-3xl font-bold">My links</p>
-          <Button>Create New Link</Button>
+          <Dialog>
+            <DialogTrigger asChild>
+              <Button>Create New Link</Button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-md">
+              <Form {...urlForm}>
+                <form
+                  onSubmit={urlForm.handleSubmit(handleUrlForm)}
+                  className="space-y-4"
+                >
+                  <FormField
+                    control={urlForm.control}
+                    name="aliase"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Aliase</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Aliase" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={urlForm.control}
+                    name="url"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Url</FormLabel>
+                        <FormControl>
+                          <Input {...field} placeholder="Your Url" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <DialogFooter>
+                    <Button type="submit">Create</Button>
+                  </DialogFooter>
+                </form>
+              </Form>
+            </DialogContent>
+          </Dialog>
         </div>
         <div className="flex w-full gap-4 items-center focus-within:ring-2 focus-within:ring-blue-400 bg-background rounded-lg p-2 border">
           <input
@@ -70,16 +154,18 @@ function UserLinks() {
         </div>
       </div>
       <div className="mt-4 flex flex-col gap-4">
-        {localUrls &&
-          localUrls.map(({ _id, aliase, redirectUrl, shortUrl, createdAt }) => (
-            <UrlCard
-              key={_id}
-              aliase={aliase}
-              redirectUrl={redirectUrl}
-              shortUrl={shortUrl}
-              createdAt={createdAt}
-            />
-          ))}
+        {(localUrls ? localUrls : urls) &&
+          (localUrls ? localUrls : urls).map(
+            ({ _id, aliase, redirectUrl, shortUrl, createdAt }) => (
+              <UrlCard
+                key={_id}
+                aliase={aliase}
+                redirectUrl={redirectUrl}
+                shortUrl={shortUrl}
+                createdAt={createdAt}
+              />
+            )
+          )}
       </div>
     </div>
   );
